e1c0126dd10239ecdb39ee0ff225131748ca54a1
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-05-03 07:50:52 +0900

    Check argument range.

--- HSPToolsVS/IntelliSense/HSPDeclarations.cs
+++ HSPToolsVS/IntelliSense/HSPDeclarations.cs
@@ -19,12 +19,15 @@ namespace HSPToolsVS.IntelliSense
 
         public override int GetCount() => _declarations.Count;
 
-        public override string GetDisplayText(int index) => _declarations[index];
+        public override string GetDisplayText(int index)
+            => 0 <= index && index < GetCount() ? _declarations[index] : null;
 
-        public override string GetName(int index) => _declarations[index];
+        public override string GetName(int index) => 0 <= index && index < GetCount() ? _declarations[index] : null;
 
         public override string GetDescription(int index)
         {
+            if (0 > index || index > GetCount())
+                return null;
             var word = _declarations[index];
             return HSPDocs.Documents.SingleOrDefault(w => w.Name == word)?.Summary;
         }
